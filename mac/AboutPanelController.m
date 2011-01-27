1fb2aff17e4360fb50569b0f0cd662628ee2719a
skierjunk@gmail.com <skierjunk@gmail.com@18a16cb9-0760-bf17-989d-930808092a9e>
2011-01-27 01:46:24 +0000

    Mac: Added GPL notices at the top of source code

--- mac/AboutPanelController.m
+++ mac/AboutPanelController.m
@@ -4,6 +4,20 @@
 //
 //  http://code.google.com/p/sms-tether
 //
+//This file is part of SMS Tether.
+//
+//SMS Tether is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//SMS Tether is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with SMS Tether.  If not, see <http://www.gnu.org/licenses/>.
 
 #import "AboutPanelController.h"
 
